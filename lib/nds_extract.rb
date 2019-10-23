c3ad195d0f061502750d63ca2fee8f3b911b65ac
Megan Wong <mwong068@gmail.com>
2019-10-23 03:55:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -73,13 +73,14 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
   
-  totals = {}
+  gross = {}
   collection.each do |movie|
-   if totals[movie[:studio]] != nil
-      totals[movie[:studio]] += movie[:worldwide_gross]
+   if gross[movie[:studio]] != nil
+      gross[movie[:studio]] += movie[:worldwide_gross]
    else
-      totals[movie[:studio]] = movie[:worldwide_gross]
- end
+      gross[movie[:studio]] = movie[:worldwide_gross]
+   end
+  end
  end
 
 def movies_with_directors_set(source)
